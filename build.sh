9f6efa6079f72f8eba7bfb4f3cd0ccc288837d58
vinhph2 <55-vinhph2@users.noreply.git.vngcloud.dev>
2021-05-19 14:54:46 +0700

    data source image

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ rm -r .terraform.lock.hcl
 go build -o terraform-provider-vserver
 OS_ARCH="$(go env GOHOSTOS)_$(go env GOHOSTARCH)"
 echo $OS_ARCH
-rm -rf ~/.terraform.d/plugins/vngcloud.com.vn/terraform/vserver/0.2
-mkdir -p  ~/.terraform.d/plugins/vngcloud.com.vn/terraform/vserver/0.2/$OS_ARCH
-mv terraform-provider-vserver ~/.terraform.d/plugins/vngcloud.com.vn/terraform/vserver/0.2/$OS_ARCH
+rm -rf ~/.terraform.d/plugins/vngcloud.vn/terraform/vserver/0.2
+mkdir -p  ~/.terraform.d/plugins/vngcloud.vn/terraform/vserver/0.2/$OS_ARCH
+mv terraform-provider-vserver ~/.terraform.d/plugins/vngcloud.vn/terraform/vserver/0.2/$OS_ARCH
 terraform init
 terraform apply
