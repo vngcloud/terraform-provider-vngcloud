80d8a8d534a53c3774a1809799b4e995438c77e7
tanvd <tanvd@vng.com.vn>
2021-08-04 18:23:32 +0700

    update url

--- examples/vdb/vdb_backup_storage/main.tf
+++ examples/vdb/vdb_backup_storage/main.tf
@@ -10,7 +10,7 @@ terraform {
 provider "vngcloud" {
   user_id = "11279"
   project_id = "pro-5c16c28a-07fa-4836-bc83-daba5a907d86"
-  vdb_base_url = /*"https://vcmc-internal.vngcloud.tech/vdb-gateway"*/"http://localhost:8100"
+  vdb_base_url = "https://vcmc-internal.vngcloud.tech/vdb-gateway"
   token_url = "https://vcmc.vngcloud.tech/user-api/v1/user/oauth2/token"
   client_id = "5V23JsOSOwM7puYnMPqxc6ibBqOlpbPI"
   client_secret = "Q4DRR473i2weaE2xmWHR5mR9fBwp8X9K"
