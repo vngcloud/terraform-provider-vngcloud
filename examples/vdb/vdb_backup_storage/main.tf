3e7bf64812ecdb1513da195247a501b005ba2454
tanvd <tanvd@vng.com.vn>
2021-08-04 18:21:45 +0700

    update create backup storage flow, add example to gen docs, refactor example folder for vdb

--- examples/vdb/vdb_backup_storage/main.tf
+++ examples/vdb/vdb_backup_storage/main.tf
@@ -10,7 +10,7 @@ terraform {
 provider "vngcloud" {
   user_id = "11279"
   project_id = "pro-5c16c28a-07fa-4836-bc83-daba5a907d86"
-  vdb_base_url = "https://vcmc-internal.vngcloud.tech/vdb-gateway"
+  vdb_base_url = /*"https://vcmc-internal.vngcloud.tech/vdb-gateway"*/"http://localhost:8100"
   token_url = "https://vcmc.vngcloud.tech/user-api/v1/user/oauth2/token"
   client_id = "5V23JsOSOwM7puYnMPqxc6ibBqOlpbPI"
   client_secret = "Q4DRR473i2weaE2xmWHR5mR9fBwp8X9K"
@@ -26,9 +26,6 @@ resource "vngcloud_vdb_backup_storage" "backup_storage_1" {
   backup_storage_package_id = data.vngcloud_vdb_backup_storage_package.backup_storage_package_1.id
   backup_storage_package_name = data.vngcloud_vdb_backup_storage_package.backup_storage_package_1.name
   engine_group = data.vngcloud_vdb_backup_storage_package.backup_storage_package_1.engine_group
-  period = 0
-  monthly_cost = 0
-  name = "relational_backup_storage"
   quota = data.vngcloud_vdb_backup_storage_package.backup_storage_package_1.quota
 }
 
@@ -42,8 +39,5 @@ resource "vngcloud_vdb_backup_storage" "backup_storage_2" {
   backup_storage_package_id = data.vngcloud_vdb_backup_storage_package.backup_storage_package_2.id
   backup_storage_package_name = data.vngcloud_vdb_backup_storage_package.backup_storage_package_2.name
   engine_group = data.vngcloud_vdb_backup_storage_package.backup_storage_package_2.engine_group
-  period = 0
-  monthly_cost = 0
-  name = "memory_store_backup_storage"
   quota = data.vngcloud_vdb_backup_storage_package.backup_storage_package_2.quota
 }
