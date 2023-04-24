ce0aa18fc40f3744d33e8f70ac9ecd291fbd5695
tunm4 <8-tunm4_2@users.noreply.git.vngcloud.dev>
2023-04-24 13:32:09 +0700

    update example

--- examples/modules/vng-cloud-vserver/provider.tf
+++ examples/modules/vng-cloud-vserver/provider.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     vngcloud = {
-      source = "registry.terraform.tunm4/vngcloud/vngcloud"
-      version = "0.2"
+      source  = "vngcloud/vngcloud"
+      version = "1.0.0"
     }
   }
 }
